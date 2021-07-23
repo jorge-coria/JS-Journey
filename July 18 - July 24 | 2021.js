8b0e06714cf3e9c21e52cdb219fab365cf53b45a
Jorge Coria <wecodethose@gmail.com>
2021-07-22 23:13:00 -0700

    Update July 18 - July 24 | 2021.js

--- July 18 - July 24 | 2021.js
+++ July 18 - July 24 | 2021.js
@@ -80,6 +80,7 @@ when server request completes, do something using the then method
 responseFromServer is set to true to represent a successful response from a server */
 
 let responseFromServer = true;
+
 makeServerRequest.then(result => {
   console.log(result);
 });
@@ -94,3 +95,53 @@ makeServerRequest.catch(error => {
   console.log(error);
 });
 
+/* Catch Off By One Errors When Using Indexing - Debugging
+In JS the last index is always one less than the length of the item */
+
+ function countToFive() {
+  let firstFive = "12345";
+  let len = firstFive.length
+  
+  for (let i = 0; i < len; i++) {
+    console.log(firstFive[i]);
+  }
+}
+
+/* Use Caution When Reinitializing Variables Inside a Loop - Debugging
+Print variable values with each cycle of your loop to uncover buggy reset-variable behavior */
+
+function zeroArray(m, n) {
+  // Creates a 2-D array with m rows and n columns of zeroes
+
+  let newArray = [];
+  let row = [];
+  for (let i = 0; i < m; i++) {
+    // Adds the m-th row into newArray
+
+    let row = [];
+    // reinitializes the row variable
+
+    for (let j = 0; j < n; j++) {
+      // Pushes n zeroes into the current row to create the columns
+
+      row.push(0);
+    }
+    // Pushes the current row, which now has n zeroes in it, to the array
+
+    newArray.push(row);
+  }
+  return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
+
+/* Prevent Infinite Loops with a Valid Terminal Condition - Debugging
+Ensure that the terminal condition is reached to prevent infinite loop. 
+Here we change i != 4 into i <= 4 and fix the infinite loop */
+
+function myFunc() {
+  for (let i = 1; i <= 4; i += 2) {
+    console.log("Still going!");
+  }
+}
