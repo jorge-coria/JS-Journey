e332834ed4afe721be1c7f0a9054874fb32bec8d
Jorge Coria <wecodethose@gmail.com>
2021-08-06 10:06:11 -0700

    Update Aug 1 - Aug 7 | 2021.js

--- Aug 1 - Aug 7 | 2021.js
+++ Aug 1 - Aug 7 | 2021.js
@@ -477,3 +477,45 @@ class MyComponent extends React.Component {
     return <div />
   }
 };
+
+/* Add Event Listeners - React
+React's synthetic event system behaves the same regardless of user browser
+Do clean-up on React components before they are unmounted and destroyed */
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ''
+    };
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress)
+  }
+
+  handleEnter() {
+    this.setState((state) => ({
+      message: state.message + 'You pressed the enter key! '
+    }));
+  }
+
+  handleKeyPress(event) {
+    if (event.keyCode === 13) {
+      this.handleEnter();
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
+};
