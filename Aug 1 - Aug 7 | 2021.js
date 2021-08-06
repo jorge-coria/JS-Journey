a71b1bf984f050d8be6d21f5092e6589a1a213d0
Jorge Coria <wecodethose@gmail.com>
2021-08-05 23:11:10 -0700

    Update Aug 1 - Aug 7 | 2021.js

--- Aug 1 - Aug 7 | 2021.js
+++ Aug 1 - Aug 7 | 2021.js
@@ -353,3 +353,42 @@ class MyForm extends React.Component {
     );
   }
 }
+
+/* Pass State as Props to Child Components - React
+Stateful Component contains state, that then renders child components
+Unidirectional Data flow - state flows one direction down the tree of app's components
+Complex stateful apps can be broken down into just a few, or a single component
+Principle of separating state logic from UI logic */
+
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'CamperBot'
+    }
+  }
+  render() {
+    return (
+       <div>
+         {/* Change code below this line */}
+         <Navbar name={this.state.name}/>
+         {/* Change code above this line */}
+       </div>
+    );
+  }
+};
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+    <div>
+      {/* Change code below this line */}
+      <h1>Hello, my name is: {this.props.name}</h1>
+      {/* Change code above this line */}
+    </div>
+    );
+  }
+};
