63435ee03673bf709bde4c680c7ac14ed20f9c4b
Jorge Coria <wecodethose@gmail.com>
2021-08-09 22:07:14 -0700

    Update Aug 8 - Aug 14 | 2021.js

--- Aug 8 - Aug 14 | 2021.js
+++ Aug 8 - Aug 14 | 2021.js
@@ -17,11 +17,9 @@ class GateKeeper extends React.Component {
     let inputStyle = {
       border: '1px solid black'
     };
-    // Change code below this line
     if (this.state.input.length > 15) {
       inputStyle.border = '3px solid red';
     }
-    // Change code above this line
     return (
       <div>
         <h3>Don't Type Too Much:</h3>
@@ -34,3 +32,151 @@ class GateKeeper extends React.Component {
     );
   }
 };
+
+/* Use Array.map() to Dynamically Render Elements - React
+Set up our component to render an unknown number of elements dynamically */
+
+const textAreaStyles = {
+  width: 235,
+  margin: 5
+};
+
+class MyToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: '',
+      toDoList: []
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleSubmit() {
+    const itemsArray = this.state.userInput.split(',');
+    this.setState({
+      toDoList: itemsArray
+    });
+  }
+  handleChange(e) {
+    this.setState({
+      userInput: e.target.value
+    });
+  }
+  render() {
+    const items = this.state.toDoList.map(i => <li>{i}</li>);
+    return (
+      <div>
+        <textarea
+          onChange={this.handleChange}
+          value={this.state.userInput}
+          style={textAreaStyles}
+          placeholder='Separate Items With Commas'
+        />
+        <br />
+        <button onClick={this.handleSubmit}>Create List</button>
+        <h1>My "To Do" List:</h1>
+        <ul>{items}</ul>
+      </div>
+    );
+  }
+}
+
+/* Give Sibling Elements a Unique Key Attribute - React
+When we create an array of elements, each one needs a key attr set to a unique value
+React will use these keys to keep track of any modification to array items
+Keys only unique between sibling elements, not globally across application */
+
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map(i => <li key={i}>{i}</li>);
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
+
+/* Use Array.filter() to Dynamically Filter an Array - React
+Filters the contents of an array based on a confidition, then returns a new array
+Here we filter users who are online, then map over filtered array and return..
+a list of usernames that are online */
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          username: 'Jeff',
+          online: true
+        },
+        {
+          username: 'Alan',
+          online: false
+        },
+        {
+          username: 'Mary',
+          online: true
+        },
+        {
+          username: 'Jim',
+          online: false
+        },
+        {
+          username: 'Sara',
+          online: true
+        },
+        {
+          username: 'Laura',
+          online: true
+        }
+      ]
+    };
+  }
+  render() {
+    const usersOnline = this.state.users.filter(user => user.online);
+    const renderOnline = usersOnline.map(user => <li key={user.username}>{user.username}</li>)
+    return (
+      <div>
+        <h1>Current Online Users:</h1>
+        <ul>{renderOnline}</ul>
+      </div>
+    );
+  }
+}
+
+/* Create a Redux Store - Redux
+In Redux there is a single state object that's responsible..
+for entire state of application. Redux store is single source of truth when it comes to app state */
+
+const reducer = (state = 5) => {
+  return state;
+}
+
+// Redux methods are available from a Redux object
+// For example: Redux.createStore()
+// Define the store here:
+let store = Redux.createStore(reducer)
+
+/* Get State from the Redux Store - Redux
+Retrieve current state held in the Redux object with getState()
+Here the code from the last challenge is written more concisely */
+
+const store = Redux.createStore(
+  (state = 5) => state
+);
+
+// Change code below this line
+const currentState = store.getState()
+
